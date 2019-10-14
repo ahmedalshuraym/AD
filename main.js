ff851256cb13ac3e8166064069bba79cb8083f2c
ahmedalshuraym <mralshuraym@gmail.com>
2019-10-14 21:06:30 +0300

    test

---
+++ main.js
@@ -0,0 +1,35 @@
+let string1  = "MyName";
+
+let variable1 = 88;
+console.log(string1);
+
+//let constant = 11;  //قيمة ثابته
+
+let firstNumber = 2;
+let secondNumber  = 4;
+
+++firstNumber
+console.log(firstNumber*secondNumber);
+console.log(string1  + '     ' + firstNumber);
+
+console.log(typeof(firstNumber)==='string');
+
+
+
+console.log("===========");
+let teststring="Bob's guitar";
+
+console.log("length" + "   "  + teststring.length);
+
+console.log(teststring.charAt(7));
+console.log(teststring.charCodeAt(5));
+console.log(teststring.includes('z'));
+console.log("split" + "   "  + teststring.split(' '));
+
+const checkstring = '#'
+console.log("repeat" + "   "  + checkstring.repeat('20'));
+
+
+
+
+
